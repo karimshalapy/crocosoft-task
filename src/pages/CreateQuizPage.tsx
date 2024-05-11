8acd9517dd828729e7326d4e1fd34a464445bfab
Karim Shalapy <karimshalapy@gmail.com>
2024-05-11 12:14:00 +0300

    feat: integrate create a quiz page

--- src/pages/CreateQuizPage.tsx
+++ src/pages/CreateQuizPage.tsx
@@ -1,8 +1,40 @@
+import QuizForm from "@/components/QuizForm/QuizForm";
 import { ROUTES } from "@/config/routes";
 import { FC } from "react";
+import { generatePath, useNavigate } from "react-router-dom";
 
 export const CreateQuizPage: FC = () => {
-  return <div>{ROUTES.createQuiz}</div>;
+  const navigate = useNavigate();
+
+  return (
+    <section className="page-container">
+      <h1>Create a Quiz</h1>
+      <QuizForm
+        onSubmit={async (values) => {
+          await fetch(`http://localhost:3000/quizes`, {
+            method: "POST",
+            body: JSON.stringify({
+              ...values,
+              created: new Date().toISOString(),
+              modified: new Date().toISOString(),
+              questions: values.questions?.map(
+                (question) =>
+                  ({
+                    answer_id: question.correctAnswerId,
+                    feedback_false: question.feedbackFalse,
+                    feedback_true: question.feedbackTrue,
+                    id: question.id,
+                    text: question.text,
+                    answers: question.answers,
+                  } || [])
+              ),
+            }),
+          });
+          navigate(generatePath(ROUTES.quizDetails, { quizId: values.id }));
+        }}
+      />
+    </section>
+  );
 };
 
 export default CreateQuizPage;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,17 @@
 import QuizesList from "@/components/QuizesList/QuizesList";
+import { ROUTES } from "@/config/routes";
 import type { QuizesResponse } from "@/types";
 import { FC } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 export const HomePage: FC = () => {
   const quizes = useLoaderData() as QuizesResponse;
   return (
     <section className="page-container">
-      <h1>Quizes</h1>
+      <div className="homepage__top-wrapper">
+        <h1>Quizes</h1>
+        <Link to={ROUTES.createQuiz}>Add a Quiz</Link>
+      </div>
       <QuizesList quizes={quizes} />
     </section>
   );
